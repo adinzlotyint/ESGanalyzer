2d7bcfb291b03305482089503d724e4c1824eb6f
adinzlotyint <adrian.kamil.zietek@gmail.com>
2025-03-25 19:41:37 +0100

    test: add unit tests for AuthService.LoginAsync (success, user not found, invalid password)

--- Tests/ESGAnalyzer.Tests.Unit/AuthServiceTests.cs
+++ Tests/ESGAnalyzer.Tests.Unit/AuthServiceTests.cs
@@ -1,11 +1,9 @@
-using Azure.Core;
 using ESGanalyzer.Backend.DTOs;
 using ESGanalyzer.Backend.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
 using Moq;
 using System.IdentityModel.Tokens.Jwt;
 
@@ -13,7 +11,6 @@ public class AuthServiceTests {
     private readonly Mock<UserManager<IdentityUser>> _mockUser;
     private readonly IConfigurationRoot _config;
     private readonly ILogger<AuthService> _logger;
-    private readonly LoginRequest _request;
 
     public AuthServiceTests() {
         _mockUser = MockUserManager();
@@ -26,7 +23,6 @@ public class AuthServiceTests {
                 { "Jwt:ExpireMinutes", "60" }
             }).Build();
         _logger = Mock.Of<ILogger<AuthService>>();
-        _request = new LoginRequest { };
     }
 
     [Fact]
@@ -38,8 +34,10 @@ public class AuthServiceTests {
             UserName = "test@example.com"
         };
 
-        _request.Email = mockUser.Email;
-        _request.Password = "validPassword";
+        var request = new LoginRequest {
+            Email = mockUser.Email,
+            Password = "validPassword"
+        };
 
         _mockUser.Setup(x => x.FindByEmailAsync(mockUser.Email)).ReturnsAsync(mockUser);
         _mockUser.Setup(x => x.CheckPasswordAsync(mockUser, "validPassword")).ReturnsAsync(true);
@@ -47,12 +45,12 @@ public class AuthServiceTests {
         var service = new AuthService(_mockUser.Object, _config, _logger);
 
         // Act
-        var token = await service.LoginAsync(_request);
+        var token = await service.LoginAsync(request);
         var jwt = new JwtSecurityTokenHandler().ReadJwtToken(token);
 
         // Assert
-        _mockUser.Verify(x => x.FindByEmailAsync(_request.Email), Times.Once());
-        _mockUser.Verify(x => x.CheckPasswordAsync(mockUser, _request.Password), Times.Once());
+        _mockUser.Verify(x => x.FindByEmailAsync(request.Email), Times.Once());
+        _mockUser.Verify(x => x.CheckPasswordAsync(mockUser, request.Password), Times.Once());
         Assert.False(string.IsNullOrWhiteSpace(token));
         Assert.Equal(mockUser.Id, jwt.Subject);
         Assert.Contains(jwt.Claims, c => c.Type == "email" && c.Value == mockUser.Email);
@@ -62,13 +60,43 @@ public class AuthServiceTests {
     public async Task LoginAsync_UserNotFound_ThrowError() {
         //Arrange
         var service = new AuthService(_mockUser.Object, _config, _logger);
-        _request.Email = "missing@example.com";
-        _mockUser.Setup(x => x.FindByEmailAsync(_request.Email)).ReturnsAsync((IdentityUser?)null);
+
+        var request = new LoginRequest {
+            Email = "test@test.pl",
+            Password = "validPassword"
+        };
+        
+        _mockUser.Setup(x => x.FindByEmailAsync(request.Email)).ReturnsAsync((IdentityUser?)null);
 
         // Act & Assert
-        var ex = await Assert.ThrowsAsync<Exception>(() => service.LoginAsync(_request));
+        var ex = await Assert.ThrowsAsync<Exception>(() => service.LoginAsync(request));
         Assert.Equal("User not found!", ex.Message);
-        _mockUser.Verify(x => x.FindByEmailAsync(_request.Email), Times.Once());
+        _mockUser.Verify(x => x.FindByEmailAsync(request.Email), Times.Once());
+    }
+
+    [Fact]
+    public async Task LoginAsync_WithInvalidPassword_ThrowError() {
+        //Arrange
+        var mockUser = new IdentityUser {
+            Id = Guid.NewGuid().ToString(),
+            Email = "test@example.com",
+            UserName = "test@example.com"
+        };
+        var service = new AuthService(_mockUser.Object, _config, _logger);
+
+        var request = new LoginRequest {
+            Email = mockUser.Email,
+            Password = "validPassword"
+        };
+
+
+        _mockUser.Setup(x => x.FindByEmailAsync(request.Email)).ReturnsAsync(mockUser);
+        _mockUser.Setup(x => x.CheckPasswordAsync(mockUser, request.Password)).ReturnsAsync(false);
+
+        // Act & Assert
+        var ex = await Assert.ThrowsAsync<Exception>(() => service.LoginAsync(request));
+        Assert.Equal("Invalid credentials", ex.Message);
+        _mockUser.Verify(x => x.CheckPasswordAsync(mockUser, request.Password), Times.Once());
     }
 
     private Mock<UserManager<IdentityUser>> MockUserManager() {
